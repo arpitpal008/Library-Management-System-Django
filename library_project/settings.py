5015e8afb58b99b8d064a4b4c4b9c47b8d00eb8e
Arpit Pal <arpitpal4554@gmail.com>
2026-06-29 20:31:44 +0530

    Disable debug for production

--- library_project/settings.py
+++ library_project/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-dev-key")
-
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
